72bc5ae3e4f1f0a9af4638661f277b079021161e
olvija1986 <164389814+olvija1986@users.noreply.github.com>
2026-03-22 23:02:24 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import bridge from '@vkontakte/vk-bridge'
 
+bridge.send('VKWebAppInit')
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
